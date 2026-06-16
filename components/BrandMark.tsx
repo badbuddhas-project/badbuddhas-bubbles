84d529f31d342a0fb425a67b152d6d613c9fdafb
Stas <stas@MacBook-Air-Stas-2.local>
2026-06-16 22:18:11 +0300

    ui: replace logo with Russian version (plohie_buddy_rus.png)

--- components/BrandMark.tsx
+++ components/BrandMark.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 export function BrandMark({ size = 24 }: { size?: number }) {
-  const width = Math.round(size * (630 / 116))
+  const width = Math.round(size * (1006 / 194))
   return (
     <img
-      src="/images/logo-white.svg"
+      src="/images/plohie_buddy_rus.png"
       alt="badbuddhas"
       width={width}
       height={size}
